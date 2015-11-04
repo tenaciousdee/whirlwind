69d1433ae05780260eff86ac6773bd0892444fec
Dennise Saxton <dennisepnd@gmail.com>
2015-11-03 18:51:21 -0600

    Finished the exercise

--- bank.rb
+++ bank.rb
@@ -1,9 +1,13 @@
 users = []
 
 5.times do 
+
+  puts "Enter your first name, last name and email"
+
   first_name = gets.chomp
   last_name = gets.chomp
   email = gets.chomp
+
 
   account_number = ""
   number = 0
@@ -13,16 +17,14 @@ users = []
     account_number << number.to_s
   end
 
-  users_hash = {"first name" => first_name, "last name" => last_name, "email" => email, "account number" => account_number}
+  users_hash = {:first_name => first_name, :last_name => last_name, :email => email, :account_number => account_number}
   users << users_hash
-  
-  users.each do |user|   
-  user.each do |h|      
-     h.each do |key,value| 
-       puts "#{key} => #{value}"
-     end
-  end
-end
-  
-end
+
+ end 
+
+
+users.each { |person| p "FIRST NAME: " + person[:first_name]; p "LAST NAME: " + person[:last_name]; p "EMAIL: " + person[:email]; p "ACCT #: " + person[:account_number] }
+
+
+
 
